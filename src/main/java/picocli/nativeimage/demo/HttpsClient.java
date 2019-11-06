db9b38de67253912edb10449775dd4d9228b9aea
Remko Popma <remkop@yahoo.com>
2019-11-07 08:53:22 +0900

    Create HttpsClient.java

---
+++ src/main/java/picocli/nativeimage/demo/HttpsClient.java
@@ -0,0 +1,102 @@
+package picocli.nativeimage.demo;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import picocli.jansi.graalvm.AnsiConsole;
+
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.io.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+
+public class HttpsClient{
+	
+   public static void main(String[] args)
+   {
+        new HttpsClient().testIt();
+   }
+	
+   private void testIt(){
+
+      String https_url = "https://www.google.com/";
+      URL url;
+      try {
+
+	     url = new URL(https_url);
+	     HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+			
+	     //dumpl all cert info
+	     print_https_cert(con);
+			
+	     //dump all the content
+	     print_content(con);
+			
+      } catch (MalformedURLException e) {
+	     e.printStackTrace();
+      } catch (IOException e) {
+	     e.printStackTrace();
+      }
+
+   }
+	
+   private void print_https_cert(HttpsURLConnection con){
+     
+    if(con!=null){
+			
+      try {
+				
+	System.out.println("Response Code : " + con.getResponseCode());
+	System.out.println("Cipher Suite : " + con.getCipherSuite());
+	System.out.println("\n");
+				
+	Certificate[] certs = con.getServerCertificates();
+	for(Certificate cert : certs){
+	   System.out.println("Cert Type : " + cert.getType());
+	   System.out.println("Cert Hash Code : " + cert.hashCode());
+	   System.out.println("Cert Public Key Algorithm : " 
+                                    + cert.getPublicKey().getAlgorithm());
+	   System.out.println("Cert Public Key Format : " 
+                                    + cert.getPublicKey().getFormat());
+	   System.out.println("\n");
+	}
+				
+	} catch (SSLPeerUnverifiedException e) {
+		e.printStackTrace();
+	} catch (IOException e){
+		e.printStackTrace();
+	}
+
+     }
+	
+   }
+	
+   private void print_content(HttpsURLConnection con){
+	if(con!=null){
+			
+	try {
+		
+	   System.out.println("****** Content of the URL ********");			
+	   BufferedReader br = 
+		new BufferedReader(
+			new InputStreamReader(con.getInputStream()));
+				
+	   String input;
+				
+	   while ((input = br.readLine()) != null){
+	      System.out.println(input);
+	   }
+	   br.close();
+				
+	} catch (IOException e) {
+	   e.printStackTrace();
+	}
+       }
+   }
+}
